cc84af04e35e3a7f0773e05d187c5bb7b3f77b32
zgs225 <zgs225@gmail.com>
2015-06-11 15:46:01 +0800

    迁移原来的测试

--- dashboard/tests/Http/Controllers/HomeControllerTest.php
+++ dashboard/tests/Http/Controllers/HomeControllerTest.php
@@ -2,23 +2,8 @@
 
 class HomeControllerTest extends TestCase
 {
-  /**
-   * @before
-   **/
-  public function setupVisit()
-  {
-    $this->call('GET', '/');
-  }
-
-  public function testVisitRootPath()
-  {
-    $this->assertResponseOk();
-  }
-
   public function testVisitRootPathShouldReturnHomeView()
   {
-    $response = $this->call('GET', '/');
-    $view     = $response->original;
-    $this->assertEquals('home', $view->name());
+    $this->visit('/')->see('布谷广告');
   }
 }
