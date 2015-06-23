cba58c5d2c5b9409acd68233858dc11da088d5df
Pcjinyan <995505775@qq.com>
2015-06-23 10:55:32 +0800

    修改首页

--- site/resources/views/home.blade.php
+++ site/resources/views/home.blade.php
@@ -19,7 +19,9 @@
                                 </button>
                                 <ul class="filter-nav-menu dropdown-menu" role="menu">
                                     <li><a href="#">北京</a> </li>
-                                    <li><a href="#">天津</a> </li>
+                                    <li><a href="#">上海</a> </li>
+                                    <li><a href="#">广州</a> </li>
+                                    <li><a href="#">深圳</a> </li>
                                 </ul>
                             </div>
                         </li>
@@ -27,28 +29,32 @@
                             <span class="filter-nav-title">媒体类型</span><br/>
                             <div class="btn-group">
                                 <button class="filter-nav-button btn btn-default dropdown-toggle" type="button" data-toggle="dropdown" aria-expanded="false">
-                                    <b>高端国际</b> <span class="filter-nav-caret caret"></span>
+                                    <b>新媒体</b> <span class="filter-nav-caret caret"></span>
                                 </button>
                                 <ul class="filter-nav-menu dropdown-menu" role="menu">
-                                    <li><a href="#">高端国际</a> </li>
-                                    <li><a href="#">奢华皇庭</a> </li>
+                                    <li><a href="#">新媒体</a> </li>
+                                    <li><a href="#">APP</a> </li>
+                                    <li><a href="#">网络</a> </li>
+                                    <li><a href="#">电视</a> </li>
                                 </ul>
                             </div>
                         </li>
                         <li class="filter-nav-item">
-                            <span class="filter-nav-title">财富圈</span><br/>
+                            <span class="filter-nav-title">社会圈</span><br/>
                             <div class="btn-group">
                                 <button class="filter-nav-button btn btn-default dropdown-toggle" type="button" data-toggle="dropdown" aria-expanded="false">
                                     <b>财富圈</b> <span class="filter-nav-caret caret"></span>
                                 </button>
                                 <ul class="filter-nav-menu dropdown-menu" role="menu">
                                     <li><a href="#">财富圈</a> </li>
-                                    <li><a href="#">行业圈</a> </li>
+                                    <li><a href="#">地产圈</a> </li>
+                                    <li><a href="#">IT圈</a> </li>
+                                    <li><a href="#">金融圈</a> </li>
                                 </ul>
                             </div>
                         </li>
                         <li class="filter-nav-item">
-                            <span class="filter-nav-title">价格</span><br/>
+                            <span class="filter-nav-title">价位</span><br/>
                             <div class="filter-nav-input">
                                 <span><input type="text"></span> <b>至</b> <span><input type="text"></span>
                             </div>
@@ -87,22 +93,22 @@
         <div class="plate">
             <div class="plate-block clearfix">
                 <div class="plate-item plate-color1">
-                    <div class="plate-item-china">免费专区活动</div>
+                    <div class="plate-item-china">免费广告位</div>
                     <div class="plate-item-big">MINIMAL OUTLINE ICONS</div>
                     <div class="plate-item-small">enjoy & share & be happy</div>
                 </div>
                 <div class="plate-item plate-color2 ml3">
-                    <div class="plate-item-china">免费专区活动</div>
+                    <div class="plate-item-china">特价广告位</div>
                     <div class="plate-item-big">MINIMAL OUTLINE ICONS</div>
                     <div class="plate-item-small">enjoy & share & be happy</div>
                 </div>
                 <div class="plate-item plate-color3 ml3">
-                    <div class="plate-item-china">免费专区活动</div>
+                    <div class="plate-item-china">特色广告位</div>
                     <div class="plate-item-big">MINIMAL OUTLINE ICONS</div>
                     <div class="plate-item-small">enjoy & share & be happy</div>
                 </div>
                 <div class="plate-item plate-color4 ml2">
-                    <div class="plate-item-china">免费专区活动</div>
+                    <div class="plate-item-china">创意类广告位</div>
                     <div class="plate-item-big">MINIMAL OUTLINE ICONS</div>
                     <div class="plate-item-small"><i>enjoy & share & be happy</i></div>
                 </div>
