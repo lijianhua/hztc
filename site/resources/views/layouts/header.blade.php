6ff9ab606d085006e4e62662b13fcb2402060156
Li Jianhua <lijianhua_oop@163.com>
2015-07-01 16:24:27 +0800

    登陆后效果

--- site/resources/views/layouts/header.blade.php
+++ site/resources/views/layouts/header.blade.php
@@ -6,12 +6,18 @@
               <li class="header-top-have fl"><a href="#"><span>我要发广告</span></a> </li>
               <li class="header-top-have fl"><a href="#"><span>我有广告位</span></a> </li>
               <li class="header-top-user fr">
-                  <a href="#"><img src="/images/user.jpg"></a>
                     @if(Auth::check())
-                        <span><a href="#">{{Auth::check()? Auth::user()->id:'登录'}}</a></span>|<span><a href="#">注册</a> </span>
+                            <a href="/users/order">
+                              <img src="/images/user_out.png">
+                            </a>
+                            <span class="header-top-user-name">
+                              欢迎 <a href="/users/order">{{Auth::user()->name}}</a> 登录
+                            </span>
+                            <span class="header-top-user-out"><a href="/auth/logout">退出</a> </span>
                     @else
-                        <span><a href="/auth/login">登录</a></span>|<span><a
-href="/auth/register">注册</a> </span>
+                        <a href="/auth/login"><img src="/images/user.jpg"></a>
+                        <span><a href="/auth/login">登录</a></span>|
+                        <span><a href="/auth/register">注册</a> </span>
                     @endif
               </li>
               <li class="fr header-top-callme">
