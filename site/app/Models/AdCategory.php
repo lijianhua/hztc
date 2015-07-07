a979d4b9ff1f16c3fc6a8c967669da3c0ca3cf41
zgs225 <zgs225@gmail.com>
2015-07-07 18:42:38 +0800

    Merge

--- site/app/Models/AdCategory.php
+++ site/app/Models/AdCategory.php
@@ -1,10 +1,16 @@
 <?php namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Iverberk\Larasearch\Traits\TransformableTrait;
+use Iverberk\Larasearch\Traits\CallableTrait;
 
-class AdCategory extends Model {
+class AdCategory extends \Baum\Node {
+  use TransformableTrait, CallableTrait;
 
-	//
-  $a = [1,2,3];
+  protected $fillable = ['name'];
 
+  public function adSpaces()
+  {
+    return $this->belongsToMany('App\Models\AdSpace', 'ad_category_ad_space');
+  }
 }
--- site/app/Models/AdSpace.php
+++ site/app/Models/AdSpace.php
@@ -2,15 +2,90 @@
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Codesleeve\Stapler\ORM\StaplerableInterface;
+use Codesleeve\Stapler\ORM\EloquentTrait;
+use Iverberk\Larasearch\Traits\SearchableTrait;
 
-class AdSpace extends Model {
+class AdSpace extends Model implements StaplerableInterface {
 
-  Use SoftDeletes;
+  Use SoftDeletes, EloquentTrait, SearchableTrait;
+
+  public static $__es_config = [
+    'autocomplete' => ['title'],
+    'suggest'      => ['title'],
+  ];
+
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+    'title', 'avatar', 'description', 'street_address',
+    'detail', 'type', 'sort'
+  ];
+
+  /**
+   * The attributes that are casts
+   *
+   * @var array
+   */
+  protected $casts = [
+    'audited' => 'boolean'
+  ];
 
   protected $dates = ['deleted_at'];
 
-  public function AdPrice()
+  public function __construct(array $attributes = array()) {
+    $this->hasAttachedFile('avatar', [
+      'styles' => [
+        'medium' => '300x300',
+        'thumb'  => '150x150',
+        'cover'  => '280x160'
+      ]
+    ]);
+
+    parent::__construct($attributes);
+  }
+
+  public function scopeWaitingForAudited($query)
   {
-    return $this->hasOne('App\Models\AdPrice');
+    return $query->whereAudited(false);
+  }
+
+  public function user()
+  {
+    return $this->belongsTo('App\Models\User');
+  }
+
+  public function categories()
+  {
+    return $this->belongsToMany('App\Models\AdCategory', 'ad_category_ad_space');
+  }
+
+  /**
+   * @follow NEVER
+   **/
+  public function images()
+  {
+    return $this->belongsToMany('App\Models\Image', 'ad_space_image', 'ad_space_id', 'image_id');
+  }
+
+  public function adPrices()
+  {
+    return $this->hasMany('App\Models\AdPrice');
+  }
+
+  public function address()
+  {
+    return $this->belongsTo('App\Models\Address');
+  }
+
+  /**
+   * @follow NEVER
+   **/
+  public function orders()
+  {
+    return $this->hasMany('App\Models\Order');
   }
 }
