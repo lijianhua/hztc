4b54dcbac74ff25d794120efbde5446ca921678f
Li Jianhua <lijianhua_oop@163.com>
2015-07-20 17:22:59 +0800

    底部

---
+++ site/app/Http/Controllers/AboutController.php
@@ -0,0 +1,57 @@
+<?php namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Session;
+use App\Models\Navigator;
+class AboutController extends Controller {
+
+  /**
+   * Create a new controller instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    // $this->middleware('auth');
+  }
+
+  /**
+   * Show the application dashboard to the user.
+   *
+   * @return Response
+   */
+  public function index()
+  {
+    $nav = '首页';
+    Session::put('current_navigator', $nav);
+    $navigators = Navigator::all()->sortBy('sort');
+    return view('about.about')->with(compact('navigators'));
+  }
+
+
+  /**
+   * Show the application dashboard to the user.
+   *
+   * @return Response
+   */
+  public function contact()
+  {
+    $nav = '首页';
+    Session::put('current_navigator', $nav);
+    $navigators = Navigator::all()->sortBy('sort');
+    return view('about.contact')->with(compact('navigators'));
+  }
+
+
+  /**
+   * Show the application dashboard to the user.
+   *
+   * @return Response
+   */
+  public function law()
+  {
+    $nav = '首页';
+    Session::put('current_navigator', $nav);
+    $navigators = Navigator::all()->sortBy('sort');
+    return view('about.law')->with(compact('navigators'));
+  }
+}
