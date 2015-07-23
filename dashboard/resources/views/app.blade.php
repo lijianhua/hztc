572747298f8fc280e256c36a468a9099130f6847
zgs225 <zgs225@gmail.com>
2015-07-23 10:40:48 +0800

    修复模板文件

--- dashboard/resources/views/app.blade.php
+++ dashboard/resources/views/app.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="zh-CN">
-<heame="csrf-token" content="{{ csrf_token() }}" />   d>
+<head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
