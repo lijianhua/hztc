b2f4a0e20180cf077726c096701ec17d117fe3e4
Li Jianhua <lijianhua_oop@163.com>
2015-07-27 10:42:51 +0800

    header update

--- site/resources/views/layouts/header.blade.php
+++ site/resources/views/layouts/header.blade.php
@@ -3,8 +3,8 @@
       <div class="header-top clearfix">
           <div class="logo fl"><a href="/"><img src="/images/logo.png"></a> </div>
           <ul class="header-top-info fr">
-              <li class="header-top-have fl"><a href="/list/all-ads"><span>我要发广告</span></a> </li>
-              <li class="header-top-have fl"><a href="http://dash.momeiw.com"><span>我有广告位</span></a> </li>
+              <li class="header-top-have fl"><a href="/list/all-ads" target="__blank"><span>我要发广告</span></a> </li>
+              <li class="header-top-have fl"><a href="http://dash.momeiw.com:8800" target="__blank"><span>我有广告位</span></a> </li>
               <li class="header-top-user fr">
                     @if(Auth::check())
                             <a href="/users/order">
