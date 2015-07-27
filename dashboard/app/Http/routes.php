d469c6d0dc02beac09b931cb11eda3004c54f8fb
zgs225 <zgs225@gmail.com>
2015-07-27 17:46:11 +0800

    更改上传地址

--- dashboard/app/Http/routes.php
+++ dashboard/app/Http/routes.php
@@ -31,7 +31,7 @@ Route::group(['middleware' => ['auth', 'auth.admin']], function () {
 
   // 轮播图中的项
   Route::delete('slides/{slide_id}/slide-items/{id}', 'SlideItemController@destroy');
-  Route::post('slides/{slide_id}/slide-items', 'SlideItemController@store');
+  Route::post('slide-items', 'SlideItemController@store');
   Route::put('slides/{slide_id}/slide-items/{id}', 'SlideItemController@update');
 
   // 广告分类管理
