c1738ddf15e2d6afb8bbc691c20462a8d600ba3e
Li Jianhua <lijianhua_oop@163.com>
2015-07-28 11:22:17 +0800

    header update

--- bussiness/resources/views/layouts/header.blade.php
+++ bussiness/resources/views/layouts/header.blade.php
@@ -26,7 +26,9 @@
               <img src="{{ $userRepons->userImageUrl(Auth::user()) }}" class="img-circle" alt="{{ Auth::user()->name }}"/>
               <p>
                 {{ Auth::user()->name }}
-                <small>{{ Auth::user()->enterprise->name }}</small>
+                @if (Auth::user()->enterprise)
+                  <small>{{ Auth::user()->enterprise->name }}</small>
+                @endif
                 <small>自{{ date('Y年, m月', strtotime(Auth::user()->created_at)) }}加入</small>
               </p>
             </li>
